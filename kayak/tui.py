49b2b51c95e05616265ebebf0971e3f6b02b6676
Saúl Piña <sauljabin@gmail.com>
2023-03-07 20:45:15 -0500

    add sidebar

--- kayak/tui.py
+++ kayak/tui.py
@@ -4,7 +4,7 @@ from textual.app import App, ComposeResult, CSSPathType
 from textual.binding import Binding
 from textual.driver import Driver
 from textual.keys import Keys
-from textual.widgets import Footer
+from textual.widgets import Footer, Tree
 
 from kayak.ksql.ksql_service import KsqlService
 from kayak.styles.colors import DESIGN
@@ -30,6 +30,8 @@ class Tui(App[None]):
         super().__init__(driver_class, css_path, watch_css)
         self.ksql_service = KsqlService(server, user, password)
         self.server = self.ksql_service.info()
+        self.topics = self.ksql_service.topics()
+        self.streams = self.ksql_service.streams()
 
     def on_mount(self) -> None:
         header = self.query_one(Header)
@@ -39,5 +41,18 @@ class Tui(App[None]):
         self.refresh_css()
 
     def compose(self) -> ComposeResult:
+        tree: Tree[dict[str, str]] = Tree("KSQL")
+        tree.show_root = False
+        tree.root.expand()
+
+        stream_node = tree.root.add("STREAMS", expand=True)
+        for stream in self.streams:
+            stream_node.add_leaf(stream.name)
+
+        topic_node = tree.root.add("TOPICS", expand=True)
+        for topic in self.topics:
+            topic_node.add_leaf(topic.name)
+
+        yield tree
         yield Header()
         yield Footer()
